791613ada22a164d8bc9472c673c729454070249
wan <wandersonsales.dev@gmail.com>
2024-04-02 11:45:15 -0300

    :lipstick: Squid theme

---
+++ .storybook/manager.tsx
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api'
+import squidTheme from './squid.theme'
+
+addons.setConfig({
+  theme: squidTheme,
+})
---
+++ .storybook/squid.theme.tsx
@@ -0,0 +1,39 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+  base: 'light',
+
+  // Typography
+  // fontBase: '"Open Sans", sans-serif',
+  // fontCode: 'monospace',
+
+  brandTitle: 'Squid',
+  brandUrl: 'https://css.squidit.com.br/',
+  brandImage: 'https://squidit.com.br/wp-content/themes/squid/assets/img/logo.svg',
+  brandTarget: '_self',
+
+  colorPrimary: '#ffffff',
+  colorSecondary: '#b61658',
+
+  // UI
+  appBg: '#ffffff',
+  appContentBg: '#ffffff',
+  // appBorderColor: '#585C6D',
+  appBorderRadius: 4,
+
+  // Text colors
+  textColor: '#10162F',
+  textInverseColor: '#ffffff',
+
+  // Toolbar default and active colors
+  barTextColor: '#9E9E9E',
+  barSelectedColor: '#585C6D',
+  barHoverColor: '#585C6D',
+  barBg: '#ffffff',
+
+  // Form colors
+  inputBg: '#ffffff',
+  // inputBorder: '#10162F',
+  inputTextColor: '#10162F',
+  inputBorderRadius: 2,
+})
