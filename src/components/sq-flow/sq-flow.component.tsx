5b39683404e21facda0f401080c87c9e2016ae24
wan <wandersonsales.dev@gmail.com>
2024-09-17 14:54:07 -0300

    :sparkles: Flow component

---
+++ src/components/sq-flow/sq-flow.component.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  activeColor?: string
+  active?: number
+  steps?: Array<{
+    status: string
+  }>
+}
+
+export default ({ className = '', activeColor = 'var(--primary_color)', steps, active, children, ...rest }: Props) => {
+  return (
+    <div className={`flow ${className}`} {...rest} style={{}}>
+      <ul>
+        {steps?.map((step, index) => {
+          return (
+            <li
+              key={index}
+              className={`${index === active ? 'active' : ''}`}
+              style={{ backgroundColor: active === index ? activeColor : '' }}
+              data-tooltip-id="tooltip"
+              data-tooltip-content={step?.status}
+            ></li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
