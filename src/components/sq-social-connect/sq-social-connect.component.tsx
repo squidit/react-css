f4151df5bc668b53bf1d7e8485c129c60736b335
Ágatha <agathamagnusg@hotmail.com>
2024-09-25 10:28:35 -0300

    ✨ feat: Update SqSocialConnect component styling

--- src/components/sq-social-connect/sq-social-connect.component.tsx
+++ src/components/sq-social-connect/sq-social-connect.component.tsx
@@ -17,9 +17,9 @@ const SqSocialConnect: React.FC<Props> = ({ profiles = [] }) => {
     <div className="social-connect">
       {profiles?.map((profile, index) => (
         <div key={profile.id || index} className="profile-container">
-          <div className="profile-wrapper" style={{ backgroundColor: profile.bgColor }}>
+          <div className="profile-wrapper">
             <img src={profile.image} alt={`${profile.name} profile`} className="profile-image" />
-            <div className="icon-container">
+            <div className="icon-container" style={{ backgroundColor: profile.bgColor }}>
               <i className="fa-brands fa-instagram gray-light"></i>
             </div>
           </div>
