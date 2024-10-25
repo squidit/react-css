56b38458a84295ab58189de1d3b827a705dba357
Ágatha <agathamagnusg@hotmail.com>
2024-10-25 15:43:56 -0300

    Update package.json version to 1.2.7 and add optional iconColor prop to SqInsufficientData component

--- src/components-creators-insights/sq-insufficient-data/sq-insufficient-data.component.tsx
+++ src/components-creators-insights/sq-insufficient-data/sq-insufficient-data.component.tsx
@@ -3,13 +3,19 @@
 import './sq-insufficient-data.component.scoped.scss'
 import { useTranslation } from 'react-i18next'
 
-export default function SqInsufficientData() {
+export interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  iconColor?: string
+}
+
+const SqInsufficientData = ({ iconColor }: Props) => {
   const { t } = useTranslation('sqInsufficientData')
 
   return (
     <div className="text-center">
-      <i className="icon-exclamation fa-regular fa-circle-exclamation mt-5"></i>
+      <i className="icon-exclamation fa-regular fa-circle-exclamation mt-5" style={{ color: iconColor || 'var(--neutral-35)' }}></i>
       <h4 className="mt-5">{t('insufficientData')}</h4>
     </div>
   )
 }
+
+export default SqInsufficientData
