60e61d4cdeb4bfd26fa99a6cdb2ad32b4517c125
João Paulo Chagas <joaopauloch17@gmail.com>
2025-06-23 12:52:38 -0300

    feat: add scoped css configs

--- src/components/sq-circle-completation/sq-circle-completation.component.tsx
+++ src/components/sq-circle-completation/sq-circle-completation.component.tsx
@@ -5,14 +5,15 @@ import defaultUser from '@assets/img/jpg/avatar.jpg'
 import React, { useEffect, useRef } from 'react'
 
 import './sq-circle-completation.component.scoped.scss'
-
+import { SqComponentWithExternalScopedCss } from '@/src'
 export interface Props extends React.HTMLAttributes<HTMLDivElement> {
   image?: string
   value?: number
   size?: 'big' | 'medium' | 'small'
+  externalCssScopedHash?: string
 }
 
-export default ({ id = '', className = '', image = '', value = 0, size = 'medium' }: Props) => {
+export default ({ id = '', className = '', image = '', value = 0, size = 'medium', externalCssScopedHash }: Props) => {
   const animateValue = (obj: HTMLSpanElement, end: number, start = 0, duration = 2500) => {
     if (obj) {
       let startTimestamp: any = null
@@ -53,35 +54,37 @@ export default ({ id = '', className = '', image = '', value = 0, size = 'medium
   }, [value])
 
   return (
-    <div id={id} className={`circle-wrap ${className} ${size}`}>
-      <div className="circle">
-        <div className={`mask full full-${finalNumber}`}>
-          <div className={`fill fill-${finalNumber}`}></div>
+    <SqComponentWithExternalScopedCss hash={externalCssScopedHash}>
+      <div id={id} className={`circle-wrap ${className} ${size}`}>
+        <div className="circle">
+          <div className={`mask full full-${finalNumber}`}>
+            <div className={`fill fill-${finalNumber}`}></div>
+          </div>
+          <div className="mask half">
+            <div className={`fill fill-${finalNumber}`}></div>
+          </div>
+          <div className={`content ${image?.length ? 'image' : ''}`}>
+            <span ref={counterRef} className={`number counter-${finalNumber}`}>
+              {finalNumber}%
+            </span>
+            <figure>
+              <img
+                src={image}
+                alt="Profile"
+                title="Profile"
+                className="img-fluid"
+                onError={({ currentTarget }) => {
+                  currentTarget.onerror = null
+                  currentTarget.src = defaultUser
+                }}
+              />
+            </figure>
+          </div>
         </div>
-        <div className="mask half">
-          <div className={`fill fill-${finalNumber}`}></div>
-        </div>
-        <div className={`content ${image?.length ? 'image' : ''}`}>
-          <span ref={counterRef} className={`number counter-${finalNumber}`}>
-            {finalNumber}%
-          </span>
-          <figure>
-            <img
-              src={image}
-              alt="Profile"
-              title="Profile"
-              className="img-fluid"
-              onError={({ currentTarget }) => {
-                currentTarget.onerror = null
-                currentTarget.src = defaultUser
-              }}
-            />
-          </figure>
-        </div>
+        <span className={`check ${finalNumber > 99 ? 'done' : ''}`}>
+          <i className="fa-regular fa-check"></i>
+        </span>
       </div>
-      <span className={`check ${finalNumber > 99 ? 'done' : ''}`}>
-        <i className="fa-regular fa-check"></i>
-      </span>
-    </div>
+    </SqComponentWithExternalScopedCss>
   )
 }
---
+++ src/hooks/use-sq-scoped-css-hash/use-sq-scoped-css-hash.hook.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+
+export const useSqScopedCssHash = (ref: React.RefObject<HTMLElement>): string | undefined => {
+  const [scopedHash, setScopedHash] = useState<string>()
+
+  useEffect(() => {
+    if (ref.current) {
+      const fullAttribute = Array.from(ref.current.attributes).find((attr: any) => attr.name.startsWith('data-v-')) as
+        | { name: string }
+        | undefined
+
+      if (fullAttribute) {
+        setScopedHash(fullAttribute.name)
+      }
+    }
+  }, [ref])
+
+  return scopedHash
+}
